cd68f4dc77bb2b0b8241d9bc022dd99e5d41f6b0
jklin <176482214@qq.com>
2018-05-30 17:15:37 +0800

    完善遥控器动画效果

--- RemoteControl/Class/TVRemote/ViewController/TVRemote/HMDTVRemoteViewController.m
+++ RemoteControl/Class/TVRemote/ViewController/TVRemote/HMDTVRemoteViewController.m
@@ -14,7 +14,10 @@
 #import "HMDTVRemoteDao.h"
 #import "HMDRemoteSettingViewController.h"
 
-@interface HMDTVRemoteViewController ()
+@interface HMDTVRemoteViewController (){
+    CGFloat maxX;
+    CGFloat maxY;
+}
 @property (nonatomic,strong) HMDTVRemoteDao *remoteDao;                 //发送按键
 @property (weak, nonatomic) IBOutlet UIView *centerKeyBoardView;        //中间键盘
 @property (weak, nonatomic) IBOutlet UIImageView *keyBoardBGImageView;
@@ -26,7 +29,8 @@
 @property (nonatomic,assign) CGFloat curVolum;                          //当前音量大小
 @property (nonatomic,weak) MPVolumeView *systemVolumeView;
 
-@property (nonatomic,weak) UIImageView *touchImageView;                 //触电位置
+@property (nonatomic,weak) UIView *touchShowView;                       //触电位置
+@property (nonatomic,strong) CAEmitterLayer *emitterLayer;              //粒子生成器
 @end
 
 @implementation HMDTVRemoteViewController
@@ -133,10 +137,15 @@
 -(void)panGesture:(UIPanGestureRecognizer *)panGesture{
     switch (panGesture.state) {
         case UIGestureRecognizerStateBegan:
-            self.touchImageView.hidden = NO;
+
+            //开启粒子动画
+            [self startEmitterAnimaton];
             break;
         case UIGestureRecognizerStateEnded:
-            self.touchImageView.hidden = YES;
+
+            //关闭粒子动画
+            [self endEmitterAnimaton];
+
             break;
         default:
             
@@ -144,11 +153,31 @@
     }
     
     CGPoint center = [panGesture locationInView:self.gestureView];
-    self.touchImageView.center = center;
-    
+
+    if (center.x < 0) {
+        center.x = 0;
+    }
+    if (center.x > maxX) {
+        center.x = maxX;
+    }
+    if (center.y < 0) {
+        center.y = 0;
+    }
+    if (center.y > maxY) {
+        center.y = maxY;
+    }
+    self.touchShowView.center = center;
+    self.emitterLayer.emitterPosition = center;
+
     CGPoint translation = [panGesture translationInView:self.gestureView];
     CGFloat x = translation.x;
+    if (center.x == 0 || center.x == maxX) {
+        x = 0;
+    }
     CGFloat y = translation.y;
+    if (center.y == 0 || center.y == maxY) {
+        y = 0;
+    }
     CGFloat sensitivity = 30.0;//灵敏度
     if (fabs(x)>sensitivity || fabs(y)>sensitivity) {
         NSInteger keyCode = 0;
@@ -201,11 +230,6 @@
 //首页
 - (IBAction)TVHomeBtnClick:(UIButton *)sender {
     [self remoteTVWithKey:KEYCODE_HOME finishBlock:nil];
-}
-
-//OK
-- (IBAction)TVOKBtnClick:(UIButton *)sender {
-    
 }
 
 //按键效果
@@ -312,6 +336,12 @@
     sender.selected = !sender.selected;
     self.centerKeyBoardView.hidden = sender.selected;
     self.gestureView.hidden = !sender.selected;
+    
+    //限制区域
+    maxX = CGRectGetWidth(self.gestureView.bounds);
+    maxY = CGRectGetHeight(self.gestureView.bounds);
+    
+    self.touchShowView.center = CGPointMake(maxX * 0.5 , maxY * 0.5);
 
 }
 -(void)remoteTVWithKey:(NSInteger)keyCode finishBlock:(HMDTVRemoteFinishBlock)block{
@@ -348,6 +378,7 @@
     NSLog(@"%@",notification);
 }
 #pragma mark - 其他
+//下载图片
 -(void)downLoadImageFromFilePath:(NSString *)filePath ip:(NSString *)ip{
     HMDWeakSelf(self)
     NSLog(@"downLoadImageFromFilePath%@",self.remoteDao);
@@ -355,6 +386,64 @@
         HMDStrongSelf(self)
         NSLog(@"还活着%@",strongSelf);
     }];
+}
+//粒子动画
+-(void)startEmitterAnimaton{
+    self.emitterLayer.birthRate = 1;
+}
+
+-(void)endEmitterAnimaton{
+    self.emitterLayer.birthRate = 0;
+}
+//外圈动画
+-(CAAnimationGroup *)getOutsideTouchImageViewAnimation{
+    CABasicAnimation *scaleAnimation_1 = [CABasicAnimation animationWithKeyPath:@"transform.scale"];
+    scaleAnimation_1.toValue = [NSNumber numberWithFloat:0.8];
+    scaleAnimation_1.duration = 1.0f;
+    scaleAnimation_1.removedOnCompletion = NO;
+    CABasicAnimation *scaleAnimation_2 = [CABasicAnimation animationWithKeyPath:@"transform.scale"];
+    scaleAnimation_2.fromValue = [NSNumber numberWithFloat:0.8];
+    scaleAnimation_2.toValue = [NSNumber numberWithFloat:1.0];
+    scaleAnimation_2.duration = 1.0f;
+    scaleAnimation_2.beginTime = 1.0;
+    scaleAnimation_2.removedOnCompletion = NO;
+    
+    CAAnimationGroup *groupAnimation = [CAAnimationGroup animation];
+    groupAnimation.animations = @[scaleAnimation_1,scaleAnimation_2];
+    
+    groupAnimation.duration = 2;
+    
+    groupAnimation.removedOnCompletion = NO;
+    // 始终保持最新的效果
+    groupAnimation.fillMode = kCAFillModeForwards;
+    groupAnimation.repeatCount = MAXFLOAT;
+    return groupAnimation;
+}
+
+//内圈动画
+-(CAAnimationGroup *)getMiddleTouchImageViewAnimation{
+    CABasicAnimation *scaleAnimation_1 = [CABasicAnimation animationWithKeyPath:@"transform.scale"];
+    scaleAnimation_1.fromValue = [NSNumber numberWithFloat:0.6];
+    scaleAnimation_1.toValue = [NSNumber numberWithFloat:1];
+    scaleAnimation_1.duration = 1.0f;
+    scaleAnimation_1.removedOnCompletion = NO;
+    CABasicAnimation *scaleAnimation_2 = [CABasicAnimation animationWithKeyPath:@"transform.scale"];
+    scaleAnimation_2.fromValue = [NSNumber numberWithFloat:1];
+    scaleAnimation_2.toValue = [NSNumber numberWithFloat:0.6];
+    scaleAnimation_2.duration = 1.0f;
+    scaleAnimation_2.beginTime = 1.0;
+    scaleAnimation_2.removedOnCompletion = NO;
+    
+    CAAnimationGroup *groupAnimation = [CAAnimationGroup animation];
+    groupAnimation.animations = @[scaleAnimation_1,scaleAnimation_2];
+    
+    groupAnimation.duration = 2;
+    
+    groupAnimation.removedOnCompletion = NO;
+    // 始终保持最新的效果
+    groupAnimation.fillMode = kCAFillModeForwards;
+    groupAnimation.repeatCount = MAXFLOAT;
+    return groupAnimation;
 }
 #pragma mark - 懒加载
 -(HMDTVRemoteDao *)remoteDao{
@@ -373,13 +462,94 @@
     return _systemVolumeView;
 }
 
--(UIImageView *)touchImageView{
-    if (_touchImageView == nil) {
-        UIImageView *touchImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 25, 25)];
-        touchImageView.image = [UIImage imageNamed:@"remote_focus"];
-        [self.gestureView addSubview:touchImageView];
-        _touchImageView = touchImageView;
+-(UIView *)touchShowView{
+    if (_touchShowView == nil) {
+        UIView *touchShowView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 35, 35)];
+        //最外层动画
+         UIImageView *touchImageView_1 = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 35, 35)];
+        touchImageView_1.image = [UIImage imageNamed:@"remote_focus_l"];
+        CAAnimationGroup *outsideTouchImageViewAnimation = [self getOutsideTouchImageViewAnimation];
+        [touchImageView_1.layer addAnimation:outsideTouchImageViewAnimation forKey:@"outsideTouchImageViewAnimation"];
+        [touchShowView addSubview:touchImageView_1];
+        //中间层动画
+         UIImageView *touchImageView_2 = [[UIImageView alloc] initWithFrame:CGRectMake(5, 5, 25, 25)];
+        touchImageView_2.image = [UIImage imageNamed:@"remote_focus_l"];
+        CAAnimationGroup *middleTouchImageViewAnimation = [self getMiddleTouchImageViewAnimation];
+        [touchImageView_2.layer addAnimation:middleTouchImageViewAnimation forKey:@"middleTouchImageViewAnimation"];
+        [touchShowView addSubview:touchImageView_2];
+        //内层
+        UIImageView *touchImageView_3 = [[UIImageView alloc] initWithFrame:CGRectMake(10, 10, 15, 15)];
+        touchImageView_3.image = [UIImage imageNamed:@"remote_focus_s"];
+        [touchShowView addSubview:touchImageView_3];
+        
+        
+        _touchShowView = touchShowView;
+        [self.gestureView addSubview:touchShowView];
     }
-    return _touchImageView;
+    return _touchShowView;
+}
+-(CAEmitterLayer *)emitterLayer{
+    if (_emitterLayer == nil) {
+        CAEmitterCell *cell = [[CAEmitterCell alloc] init];
+        //展示的图片
+        UIImage *cellImage = [UIImage imageNamed:@"remote_focus_l"];
+        cell.contents = (__bridge id _Nullable)(cellImage.CGImage);
+        
+        //每秒粒子产生个数的乘数因子，会和layer的birthRate相乘，然后确定每秒产生的粒子个数
+        cell.birthRate = 30;
+        //每个粒子存活时长
+        cell.lifetime = 0.8;
+        //粒子生命周期范围
+        //    cell.lifetimeRange = 0;
+        //粒子透明度变化，设置为－0.4，就是每过一秒透明度就减少0.4，这样就有消失的效果,一般设置为负数。
+        cell.alphaSpeed = -1;
+        //    cell.alphaRange = 0.5;
+        //粒子的速度
+        cell.velocity = 0;
+        //粒子的速度范围
+        cell.velocityRange = 0;
+        //周围发射的角度，如果为M_PI*2 就可以从360度任意位置发射
+        //cell.emissionRange = M_PI*2;
+        //粒子内容的颜色
+        //    cell.color = [[UIColor whiteColor] CGColor];
+        
+        //设置了颜色变化范围后每次产生的粒子的颜色都是随机的
+        //    cell.redRange = 0.5;
+        //    cell.blueRange = 0.5;
+        //    cell.greenRange = 0.5;
+//        CGFloat scale = (CGFloat)(35.0/cellImage.size.width);
+        //缩放比例
+        cell.scale = 0.5;
+        cell.scaleSpeed = -0.8;
+        //缩放比例范围
+//            cell.scaleRange = 0.02;
+        
+        //粒子的初始发射方向
+        //    cell.emissionLongitude = M_PI;
+        //Y方向的加速度
+        cell.yAcceleration = 0;
+        //X方向加速度
+        //    cell.xAcceleration = 20.0;
+        
+        _emitterLayer = [CAEmitterLayer layer];
+        
+        //发射位置
+        _emitterLayer.emitterPosition = CGPointZero;
+        //粒子产生系数，默认为1
+        _emitterLayer.birthRate = 1;
+        //发射器的尺寸
+        //    _emitterLayer.emitterSize = CGSizeMake(SCREEN_WIDTH, 0);
+        //发射的形状
+        _emitterLayer.emitterShape = kCAEmitterLayerLine;
+        //发射的模式
+        _emitterLayer.emitterMode = kCAEmitterLayerLine;
+        //渲染模式
+        _emitterLayer.renderMode = kCAEmitterLayerOldestFirst;
+        _emitterLayer.masksToBounds = NO;
+        //_emitterLayer.zPosition = -1;
+        _emitterLayer.emitterCells = @[cell];
+        [self.gestureView.layer addSublayer:_emitterLayer];
+    }
+    return _emitterLayer;
 }
 @end
